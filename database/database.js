c0a57490ba66c6a5229ffad6927db1f78d09623d
Messiaslogia <messias.logia@gmail.com>
2024-03-21 12:52:19 -0300

    Deletando artigos

--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize('blogNode','root','',{
     host: 'localhost',
     dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
