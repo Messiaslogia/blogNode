acd8f9a61bdd5c74ad735887105431c5037a61c1
Messiaslogia <messias.logia@gmail.com>
2024-04-23 10:31:45 -0300

    Finalizando projeto BlogNode!

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParse = require("body-parser")
 const connection = require("./database/database");
+const session = require("express-session");   
 
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
@@ -16,6 +17,14 @@ const User = require("./user/User");
 
 // View engine
 app.set('view engine', 'ejs');
+
+// Sessions
+app.use(session({
+    secret: "literalmentequalquercoisa",
+    cookie: {maxAge: 30000},
+    resave: false, 
+    saveUninitialized: false
+}))
 
 // Static
 app.use(express.static('public'));
@@ -37,6 +46,8 @@ connection.authenticate()
 app.use("/", categoriesController);
 app.use("/", articlesController);
 app.use("/", usersController);
+
+
 
 
 app.get("/", (req, res) =>{
